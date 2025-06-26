4b30d1a3ef88c694a60d9bf997347ba4d5ad0008
Woulf <contact@woulf.dev>
2025-06-26 12:12:12 +0200

    Update vehicle.py
    Added vehicle base modell.

--- backend/app/models/vehicle.py
+++ backend/app/models/vehicle.py
@@ -1,13 +1,20 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, date
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
 from app.core.config import settings
 
 class VehicleBase(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID = Field()
     make: str = Field()
     model: str = Field()
+    production_year: date = Field()
+    horsepower: int = Field()
+    torque: int = Field()
+    mileage: int = Field()
+    max_speed: int = Field()
+    creation_date: datetime = Field()
 
-class Vehicle(VehicleBase):
-    user_id: uuid.UUID
+class Vehicle(VehicleBase, Table=True):
+    test: bool
